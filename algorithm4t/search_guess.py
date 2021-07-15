3927fae61a514891375483100f20805207ab894c
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2021-07-16 01:23:36 +0800

    redraw_bar

--- algorithm4t/search_guess.py
+++ algorithm4t/search_guess.py
@@ -27,17 +27,35 @@ class SearchGuess:
     LOGO_Y = 0
 
     RULER_X = 150
-    RULER_Y = 150
+    RULER_Y = 160
 
     PUZZLE_X = 200
     PUZZLE_Y = 90
+
+    BAR_COLOR = '#349beb'
+    
+
+    BAR_X = 160
+    BAR_WIDTH = 30
+    BAR_X_RIGHT = BAR_X + BAR_WIDTH
+    BAR_MIN_Y = 205
+    BAR_MAX_Y = 680
+    BAR_MAX_HEIGHT = BAR_MAX_Y - BAR_MIN_Y
+    THIN_BAR_WIDTH = 5
+    THIN_BAR_GAP = 3
+    THIN_BAR_X = BAR_X_RIGHT + THIN_BAR_GAP
+    THIN_BAR_X_RIGHT = THIN_BAR_X + THIN_BAR_WIDTH
 
     
     def __init__(self):
         self.puzzle_answer = None  # bin str
         self.puzzle_lower_bound = None
         self.puzzle_upper_bound = None
-        
+        self.answer_lower_bound = None
+        self.answer_upper_bound = None
+        self.puzzle_bound_delta = None
+        self.bar_id = None
+        self.thin_bar_id = None
 
     def 產生題目(self, *args, **kwargs):
         if common.current_algorithm is not None and common.current_algorithm != self.ALGORITHM_NAME :
@@ -46,7 +64,10 @@ class SearchGuess:
 
         if len(args) == 0:
             self.puzzle_lower_bound = self.DEFAULT_LOWER_BOUND
+            
             self.puzzle_upper_bound = self.DEFAULT_UPPER_BOUND
+            
+            self.puzzle_bound_delta = self.puzzle_upper_bound - self.puzzle_lower_bound
             if '隨機種子' in kwargs:
                 #print('seed: ', kwargs['隨機種子'])
                 random.seed(kwargs['隨機種子'])
@@ -60,11 +81,72 @@ class SearchGuess:
 
         self.canvas.update()
 
+    def delay(self):
+        #pass
+        time.sleep(0.0001)  
+
     def _do_init(self):
         self.gui_init()
         self.set_background()
         self.set_logo()
         self.set_assets()
+        self.bar_init()
+        #self.adjsut_lower_bound(new_y)
+
+        
+    def bar_init(self):
+        # bar 
+        for i in range(1,60, 1):
+            self.redraw_bar(0, i)
+
+    def redraw_bar(self, lower_num, upper_num):
+        if lower_num == upper_num :
+            print('redraw_bar: lowernum == upper_num')
+            return
+
+        if lower_num > upper_num:
+            raise 搜尋猜數錯誤 
+
+        if self.bar_id is not None:
+            self.canvas.delete(self.bar_id)
+            self.bar_id = None
+
+        if self.thin_bar_id is not None:
+            self.canvas.delete(self.thin_bar_id)
+            self.thin_bar_id = None
+
+        big_y = self.num2y(lower_num)
+        small_y = self.num2y(upper_num)
+
+        self.bar_id = self.canvas.create_rectangle(
+                        self.BAR_X, 
+                        small_y,
+                        self.BAR_X_RIGHT, 
+                        big_y,
+                        width=0,    
+                        fill=self.BAR_COLOR,
+                    )
+
+        self.thin_bar_id = self.canvas.create_rectangle(
+                        self.THIN_BAR_X, 
+                        small_y,
+                        self.THIN_BAR_X_RIGHT, 
+                        big_y,
+                        width=0,    
+                        fill=self.BAR_COLOR,
+                    )
+
+        self.canvas.update()
+        self.delay()
+
+    def adjust_lower_bound(self, new_y):
+        pass
+
+
+    def num2y(self, n):
+        # to do :value check
+        tmp =  self.BAR_MAX_Y - n* self.BAR_MAX_HEIGHT / self.puzzle_bound_delta 
+        return int(tmp)
 
     def gui_init(self):
         self.root = tk.Tk()
@@ -104,6 +186,15 @@ class SearchGuess:
         
         # load arrow and hide
 
+         
+        # self.bar_id = self.canvas.create_rectangle(
+        #                 self.BAR_X, 
+        #                 self.BAR_MIN_Y,
+        #                 self.BAR_X + self.BAR_WIDTH, 
+        #                 self.BAR_MAX_Y,
+        #                 width=0,    
+        #                 fill='#349beb',
+        #             ) 
 
     def set_background(self):
         path = Path(__file__).parent / 'images' / (self.BACKGROUND_NAME + '.png')
@@ -131,3 +222,4 @@ class SearchGuess:
                 )
 
 搜尋猜數 = SearchGuess()  
+
